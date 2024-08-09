5da69816b5039baa693d920d73ca5f47b3ab4534
Vitor Carvalho <lightningspirit@gmail.com>
2024-08-09 14:22:41 +0100

    (feat): import articles from old blog

--- src/components/posts/index.tsx
+++ src/components/posts/index.tsx
@@ -6,8 +6,10 @@ export function Posts({ posts }: { posts: Post[] }) {
     <ul className="!list-none !list-outside !pl-0 !table">
       {posts.map(({ slug, title, description, date }) => (
         <li key={slug} className="table-row">
-          <p className="text-sm !text-zinc-500 proportional-nums lg:pt-3 w-24">
-            {new Date(date).toLocaleDateString()}{" "}
+          <p className="text-sm !text-zinc-500 proportional-nums lg:pt-3 w-32">
+            {new Date(date).toLocaleDateString("en-US", {
+              dateStyle: "medium",
+            })}{" "}
           </p>
           <div className="space-y-1 lg:table-cell">
             <h2 className="font-serif text-xl">
