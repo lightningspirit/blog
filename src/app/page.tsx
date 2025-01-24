c10257aba474120df7c55c789a9bc3012aa1e338
Vitor Carvalho <lightningspirit@gmail.com>
2025-01-24 15:16:46 +0000

    chore(hp): update working position

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
         Hi, I&#39;m <b>Vitor de Carvalho</b>, a Software Engineer, Hacker, <a href="https://github.com/lightningspirit" rel="nofollow noopener noreferrer">Open Source contributor</a>, Musician, and Astrophysics enthusiast living in sunny <a href="https://duckduckgo.com/?q=lisbon+portugal" rel="nofollow noopener noreferrer">Lisbon, Portugal</a>.
       </p>
       <p>
-        Previously, I&#39;ve worked for <a href="https://www.sapo.pt/" rel="nofollow noopener noreferrer">SAPO</a> and <a href="https://www.talkdesk.com/" rel="nofollow noopener noreferrer">Talkdesk</a>. Currently, I&#39;m developing a range of exciting products at <a href="https://moveyourdigital.com/" rel="nofollow noopener noreferrer">Move Your Digital</a>, a remote-first digital agency.
+      I work at <a href="https://clickhouse.com/" rel="nofollow noopener noreferrer">ClickHouse</a> and in the past, I worked for <a href="https://www.sapo.pt/" rel="nofollow noopener noreferrer">SAPO</a> and <a href="https://www.talkdesk.com/" rel="nofollow noopener noreferrer">Talkdesk</a>
       </p>
       <p>
         Some of my personal projects include <a href="https://www.npmjs.com/package/funtastic">Funtastic</a> and <a href="https://www.npmjs.com/package/editorjs-blocks-react-renderer">Editor.js Blocks React Renderer</a>
